5597585277cca3545736892c98be7ad7a04137d4
Tiago H. Piantavinha <email@email.com>
2025-04-24 12:22:46 -0300

    Correção de rota e visualização em mobile

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 botao_listar_categorias_click = function() {
-    navegarPara('modulos/produtos/categorias/lista_categorias.html');
+    navegarPara('./modulos/produtos/categorias/lista_categorias.html');
 }

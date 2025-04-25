e723ec37c73397b255a854de7e3d550b8da40b3f
Tiago H. Piantavinha <email@email.com>
2025-04-24 21:11:34 -0300

    Correção do nome da tela de moedas e novos botões na homes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 botao_listar_categorias_click = function() {
     navegarPara('./modulos/produtos/categorias/lista_categorias.html');
 }
+
+botao_listar_medidas_click = function() {
+    navegarPara('./modulos/produtos/medidas/tela_medida.html');
+}
+
+botao_listar_moedas_click = function() {
+    navegarPara('./modulos/produtos/moedas/tela_de_moeda.html');
+}

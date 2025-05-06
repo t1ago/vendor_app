3d2d27219bf7cb6a6eb53b14a0e9aed53ef05782
MigueLendo <miguelsaccoo@gmail.com>
2025-05-06 19:04:16 -0300

    Alterações da tela de moeda

--- modulos/produtos/moedas/tela_de_moeda.js
+++ modulos/produtos/moedas/tela_de_moeda.js
@@ -2,16 +2,10 @@ campo_id = document.getElementById("id")
 campo_nome = document.getElementById("nome")
 campo_moeda = document.getElementById("moeda")
 
-botao_salvar_click = function () {
+
+function cadastrar_moeda() {
     if (window.localStorage.getItem("moeda") == null) {
         window.localStorage.setItem("moeda", "[]")
-    }
-}
-
-
-botao_cancelar_click = function () {
-    if (window.location.href("voltar") == null) {
-
     }
 
     moedas = window.localStorage.getItem("moeda")
@@ -23,8 +17,51 @@ botao_cancelar_click = function () {
     }
     if (listasmoedas.length == 0) {
         moeda.id = 1
+    } else {
+        listasmoedas_ordenada = listasmoedas.sort(
+            function (a, b) {
+                return a - b
+            }
+        ).reverse()
+        ultimamoeda = listasmoedas_ordenada[0]
+        moeda.id = ultimamoeda.id + 1
     }
+    campo_id.value = moeda.id
+
+
     listasmoedas.push(moeda)
     moedas = JSON.stringify(listasmoedas)
     window.localStorage.setItem("moeda", moedas)
 }
+alterar_moeda = function () {
+    exibir_mensagem_operacao(true, 'Alterando moeda', null)
+
+    if (window.localStorage.getItem('moedas') == null) {
+        window.localStorage.setItem('moedas', '[]')
+    }
+
+    listasmoedas = JSON.parse(window.localStorage.getItem('moedas'))
+
+    if (listasmoedas.length == 0) {
+        criar_moeda()
+    } else {
+
+        indice = listasmoedas.findIndex(function (valor, array_indice, array) {
+            return valor.id == campo_id.value
+        })
+        moeda = listasmoedas[indice]
+        moeda.nome = campo_nome.value
+        listasmoedas[indice] = categoria
+
+        window.localStorage.setItem('moedas', JSON.stringify(listasmoedas))
+
+        exibir_mensagem_operacao(true, 'moeda alterada com Sucesso', 'sucesso')
+        setInterval(function () {
+            botao_cancelar_click()
+        }, 2000)
+    }
+}
+
+
+
+

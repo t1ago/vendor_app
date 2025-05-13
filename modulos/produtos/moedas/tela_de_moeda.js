c39084f506a3b1c7f6f7dbfb344c9889dbbfeb6c
MigueLendo <miguelsaccoo@gmail.com>
2025-05-13 20:56:46 -0300

    Finalização, Encerramento

--- modulos/produtos/moedas/tela_de_moeda.js
+++ modulos/produtos/moedas/tela_de_moeda.js
@@ -2,6 +2,10 @@ campo_id = document.getElementById("id")
 campo_nome = document.getElementById("nome")
 campo_moeda = document.getElementById("moeda")
 
+function voltarmoeda() {
+    window.location.href = "lista_moeda.html"; // redireciona para a lista
+}
+
 
 function cadastrar_moeda() {
     if (window.localStorage.getItem("moeda") == null) {
@@ -33,35 +37,60 @@ function cadastrar_moeda() {
     moedas = JSON.stringify(listasmoedas)
     window.localStorage.setItem("moeda", moedas)
 }
+
+
 alterar_moeda = function () {
-    exibir_mensagem_operacao(true, 'Alterando moeda', null)
+    moedas = window.localStorage.getItem("moeda")
 
-    if (window.localStorage.getItem('moedas') == null) {
-        window.localStorage.setItem('moedas', '[]')
+    listasmoedas = JSON.parse(moedas)
+
+
+    indice = listasmoedas.findIndex(function (valor, array_indice, array) {
+        return valor.id == campo_id.value
+    })
+
+    moeda = listasmoedas[indice]
+    moeda.nome = campo_nome.value
+    moeda.moeda = campo_moeda.value
+    listasmoedas[indice] = moeda
+
+
+    window.localStorage.setItem('moeda', JSON.stringify(listasmoedas))
+
+}
+
+botao_salvar_click = function () {
+    if (campo_id.value == "") {
+        cadastrar_moeda()
     }
+    else {
+        alterar_moeda()
+    }
+    window.location.href = "lista_moeda.html"
+}
 
-    listasmoedas = JSON.parse(window.localStorage.getItem('moedas'))
+function exibirdados() {
+    let parametros = window.location.search
 
-    if (listasmoedas.length == 0) {
-        criar_moeda()
-    } else {
+    if (parametros) {
+        // ?id=10
+        parametrosquebrado = new URLSearchParams(parametros)
+        // [id, 10]
+        parametrosid = parametrosquebrado.get("id")
+        // 10
+        let moedas = window.localStorage.getItem("moeda")
 
-        indice = listasmoedas.findIndex(function (valor, array_indice, array) {
-            return valor.id == campo_id.value
+        let listasmoedas = JSON.parse(moedas)
+
+        let localizado = listasmoedas.find(function (item, array_indice, array) {
+            return item.id == parametrosid
         })
-        moeda = listasmoedas[indice]
-        moeda.nome = campo_nome.value
-        listasmoedas[indice] = categoria
-
-        window.localStorage.setItem('moedas', JSON.stringify(listasmoedas))
-
-        exibir_mensagem_operacao(true, 'moeda alterada com Sucesso', 'sucesso')
-        setInterval(function () {
-            botao_cancelar_click()
-        }, 2000)
+        campo_id.value = localizado.id
+        campo_nome.value = localizado.nome
+        campo_moeda.value = localizado.moeda
     }
 }
 
-
+exibirdados()
 
 

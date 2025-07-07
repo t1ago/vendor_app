ac901e03c7a4590da60450ace7757ff8a4c586ff
Tiago H. Piantavinha <tiago.henrip@hotmail.com>
2025-07-07 18:00:23 -0300

    Ajustes de path

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 
-botao_listar_categorias_click = function() {
-    navegarPara('./modulos/produtos/categorias/lista_categorias.html');
+botao_listar_categorias_click = function () {
+    navegarPara('./src/produtos/categorias/lista_categorias.html');
 }
 
-botao_listar_cores_click = function() {
-    navegarPara('./modulos/produtos/cores/lista_cores.html');
+botao_listar_cores_click = function () {
+    navegarPara('./src/produtos/cores/lista_cores.html');
 }
 
-botao_listar_medidas_click = function() {
-    navegarPara('./modulos/produtos/medidas/lista_medidas.html');
+botao_listar_medidas_click = function () {
+    navegarPara('./src/produtos/medidas/lista_medidas.html');
 }
 
-botao_listar_moedas_click = function() {
-    navegarPara('./modulos/produtos/moedas/tela_de_moeda.html');
+botao_listar_moedas_click = function () {
+    navegarPara('./src/produtos/moedas/tela_de_moeda.html');
 }

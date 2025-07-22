84d44416398f4e10b96280735c1393bff1df3188
VictorHCamargo <victor.h0786@gmail.com>
2025-07-22 20:23:29 -0300

    Feito buscar categorias na aula 22/07

--- src/produtos/fornecedores/victor/fornecedor_victor.js
+++ src/produtos/fornecedores/victor/fornecedor_victor.js
@@ -11,12 +11,13 @@ valores_dados = {
     descricao:null
 }
 
-function focar_campo(elemento) {
+async function focar_campo(elemento,minha_funcao) {
     let elemento_pai = elemento.parentNode;
     let elemento_informacao = elemento_pai.getElementsByClassName('esconder')[0]
     if (elemento_informacao != undefined) {
         elemento_informacao.classList.remove('esconder')
     }
+    await minha_funcao()
 }
 function desfocar_campo(event) {
     if (event?.target?.closest('.informacao-lista') || event?.target?.closest('input')) {
@@ -39,4 +40,47 @@ function esconder_informacao(elemento) {
     if (elemento_pai != undefined) {
         elemento_pai.classList.add('esconder')
     }
+}
+async function buscar_informacao_categorias() {
+    info_categorias = document.getElementById("info_categorias")
+    info_categorias.innerHTML = ""
+    
+    requisicao = await fetch(`http://localhost:3000/categorias`,{
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        }
+    })
+    if (requisicao.ok ==true) {
+        lista = document.createElement("ul")
+        lista_categorias = await requisicao.json()
+        lista_categorias.data.forEach(item => {
+            //Puxa o id
+            lista_item = document.createElement("li")
+            lista_item.dataset.id = item.id
+            span_item = document.createElement("span")
+            span_item.innerHTML = item.nome
+            lista_item.appendChild(span_item)
+
+            lista_item.onclick = function() {
+                selecionar_item(lista_item, 'id_categoria')
+            }
+            lista.appendChild(lista_item)
+        })
+        info_categorias.appendChild(lista)
+    } else {
+
+    }
+}
+async function buscar_informacao_grupo() {
+    info_grupos = document.getElementById("info_grupo")
+    info_grupos.innerHTML = ""
+    requisicao = await fetch(`http://localhost:3000/grupos`,{
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        }
+    })
 }

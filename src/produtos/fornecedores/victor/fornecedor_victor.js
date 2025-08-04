2f44704286d056b675d87e6002fd788625e3760d
VictorHCamargo <victor.h0786@gmail.com>
2025-08-04 19:43:23 -0300

    Continuação da construção da tela fornecedor durante a aula do dia 29/07

--- src/produtos/fornecedores/victor/fornecedor_victor.js
+++ src/produtos/fornecedores/victor/fornecedor_victor.js
@@ -17,6 +17,7 @@ async function focar_campo(elemento,minha_funcao) {
     if (elemento_informacao != undefined) {
         elemento_informacao.classList.remove('esconder')
     }
+    
     await minha_funcao(elemento)
 }
 function desfocar_campo(event) {
@@ -42,53 +43,33 @@ function esconder_informacao(elemento) {
     }
 }
 async function buscar_informacao_categorias(elemento) {
-    info_categorias = document.getElementById("info_categorias")
-    info_categorias.innerHTML = ""
-    
-    requisicao = await fetch(`http://localhost:3000/categorias`,{
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json'
-        }
-    })
-    if (requisicao.ok ==true) {
-        lista = document.createElement("ul")
-        lista_categorias = await requisicao.json()
-        lista_categorias.data.forEach(item => {
-            //Puxa o id
-            lista_item = document.createElement("li")
-            lista_item.dataset.id = item.id
-            span_item = document.createElement("span")
-            span_item.innerHTML = item.nome
-            lista_item.appendChild(span_item)
+    try {
+        info_categorias = document.getElementById("info_categorias")
+        info_categorias.innerHTML = ""
 
-            lista_item.onclick = function() {
-                selecionar_item(lista_item, 'id_categoria')
-                adicionar_item_campo(elemento,item.nome)
+        let elemento_carregando = document.createElement("span")
+        elemento_carregando.innerHTML = "Carregando..."
+
+        let div_resultado = document.createElement("div")
+        div_resultado.style.color = "white"
+        div_resultado.appendChild(elemento_carregando)
+
+        info_categorias.appendChild(div_resultado)
+
+
+        requisicao = await fetch(`http://localhost:3000/categorias`,{
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
             }
-            lista.appendChild(lista_item)
         })
-        info_categorias.appendChild(lista)
-    } else {
-
-    }
-}
-async function buscar_informacao_grupos(elemento) {
-    info_grupos = document.getElementById("info_grupos")
-    info_grupos.innerHTML = ""
-    requisicao = await fetch(`http://localhost:3000/grupos`,{
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json'
-        }
-    })
-    if(requisicao.ok == true) {
-        lista = document.createElement("ul")
-        listas_grupos = await requisicao.json()
-        listas_grupos.data.filter(item => {
-            return item.nome != null}).forEach(item => {
+        div_resultado.remove()
+        if (requisicao.ok ==true) {
+            lista = document.createElement("ul")
+            lista_categorias = await requisicao.json()
+            lista_categorias.data.forEach(item => {
+                //Puxa o id
                 lista_item = document.createElement("li")
                 lista_item.dataset.id = item.id
                 span_item = document.createElement("span")
@@ -101,10 +82,76 @@ async function buscar_informacao_grupos(elemento) {
                 }
                 lista.appendChild(lista_item)
             })
-            info_grupos.appendChild(lista)
-    } else {
+            info_categorias.appendChild(lista)
+        }
+    } catch {
+        info_categorias = document.getElementById("info_categorias")
+        info_categorias.innerHTML = ""
 
-    }
+        let elemento_erro = document.createElement("span")
+        elemento_erro.innerHTML = "Nenhuma categoria foi encontrada..."
+
+        let div_resultado = document.createElement("div")
+        div_resultado.style.color = "white"
+        div_resultado.appendChild(elemento_erro)
+
+        info_categorias.appendChild(div_resultado)
+    } 
+}
+async function buscar_informacao_grupos(elemento) {
+    try {
+        info_grupos = document.getElementById("info_grupos")
+        info_grupos.innerHTML = ""
+        
+        let elemento_carregando = document.createElement("span")
+        elemento_carregando.innerHTML = "Carregando..."
+
+        let div_resultado = document.createElement("div")
+        div_resultado.style.color = "white"
+        div_resultado.appendChild(elemento_carregando)
+
+        info_grupos.appendChild(div_resultado)
+
+        requisicao = await fetch(`http://localhost:3000/grupos`,{
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        })
+        div_resultado.remove()
+        if(requisicao.ok == true) {
+            lista = document.createElement("ul")
+            listas_grupos = await requisicao.json()
+            listas_grupos.data.filter(item => {
+                return item.nome != null}).forEach(item => {
+                    lista_item = document.createElement("li")
+                    lista_item.dataset.id = item.id
+                    span_item = document.createElement("span")
+                    span_item.innerHTML = item.nome
+                    lista_item.appendChild(span_item)
+
+                    lista_item.onclick = function() {
+                        selecionar_item(lista_item, 'id_categoria')
+                        adicionar_item_campo(elemento,item.nome)
+                    }
+                    lista.appendChild(lista_item)
+                })
+                info_grupos.appendChild(lista)
+        } 
+    } catch {
+        info_grupos = document.getElementById("info_categorias")
+        info_grupos.innerHTML = ""
+
+        let elemento_erro = document.createElement("span")
+        elemento_erro.innerHTML = "Nenhum grupo foi encontrado..."
+
+        let div_resultado = document.createElement("div")
+        div_resultado.style.color = "white"
+        div_resultado.appendChild(elemento_erro)
+
+        info_grupos.appendChild(div_resultado)
+    }  
 }
 async function buscar_informacao_marca(elemento) {
     let info_marca = document.getElementById("info_marca")

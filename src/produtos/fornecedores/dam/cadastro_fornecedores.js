595831e0cb2b1004e2977fe8f865a1e9eec6f971
Damilo Queiroz <damilo.queiroz@gmail.com>
2025-09-02 18:58:31 -0500

    correção de bugs do fornecdores

--- src/produtos/fornecedores/dam/cadastro_fornecedores.js
+++ src/produtos/fornecedores/dam/cadastro_fornecedores.js
@@ -102,9 +102,27 @@ async function buscarAPI(elemento, endereco) {
           infos.campo_nome = 'nome'
           infos.campo_salvar = 'id_moeda'
           break;
-      
-        default:
+        case 'grupos':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_grupo'
           break;
+        case 'medidas':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_unidade_medida'
+          break;
+        case 'marca':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_marca'
+          break;
+        case 'cores':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'hexadecimal'
+          infos.campo_salvar = 'id_cor'
+          break;
+        default:
       }
 
       criar_lista_informacao(dados, infos.campo_nome, infos.campo_id, infos.campo_salvar, elemento, info_div);
@@ -118,12 +136,14 @@ async function buscarAPI(elemento, endereco) {
   }
 }
 
-async function buscar_informacao_grupos(elemento) {
-  info_grupos = document.getElementById('info-grupos')
-  carregar_dados(info_grupos)
+async function buscar_informacao_grupos(elemento, endereco) {
+  elemento_pai = elemento.parentNode
+
+  info_div = elemento_pai.getElementsByTagName("div") [0]
+  carregar_dados(info_div);
 
   try {
-    requisicao = await fetch(`${API_HOST}/grupos`, {
+    requisicao = await fetch(`${API_HOST}/${endereco}`, {
       method: "GET",
       headers: {
         'Accept': 'application/json',
@@ -132,17 +152,58 @@ async function buscar_informacao_grupos(elemento) {
     })
 
     if (requisicao.ok == true) {
-      carregar_dados(info_grupos, false);
+      carregar_dados(info_div, false);
 
-      dados = await requisicao.json();
-      criar_lista_informacao(dados, 'nome', 'id', 'id_grupo', elemento, info_grupos);
+            dados = await requisicao.json();
+
+      infos = {
+        campo_id: null,
+        campo_nome: null,
+        campo_salvar: null
+      }
+      
+      switch (endereco) {
+        case 'categorias': 
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_categoria'
+          break;
+        case 'moedas': 
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_moeda'
+          break;
+        case 'grupos':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_grupo'
+          break;
+        case 'medidas':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_unidade_medida'
+          break;
+        case 'marca':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_marca'
+          break;
+        case 'cores':
+          infos.campo_id = 'id'
+          infos.campo_nome = 'nome'
+          infos.campo_salvar = 'id_cor'
+          break;
+        default:
+      }
+
+      criar_lista_informacao(dados, infos.campo_nome, infos.campo_id, infos.campo_salvar, elemento, info_div);
     } else {
-      carregar_dados(info_categorias, false);
-      exibir_informacao_falha(info_grupos, 'Nenhuma informação localizada.');
+      carregar_dados(info_div, false);
+      exibir_informacao_falha(info_div, 'Nenhuma informação localizada.');
     }
   } catch (erro) {
-    carregar_dados(info_categorias, false);
-    exibir_informacao_falha(info_grupos, 'Nenhumma informação localizada.');
+    carregar_dados(info_div, false);
+    exibir_informacao_falha(info_div, 'Nenhumma informação localizada.');
   }
 }
 
@@ -185,3 +246,4 @@ function criar_lista_informacao(dados, campo_nome, campo_id, campo_salvar, eleme
 adicionar_valor_campo = function (elemento, valor) {
     elemento.value = valor
 }
+
